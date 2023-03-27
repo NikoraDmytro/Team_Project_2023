9642afc9c80d879b2ec6980fbb49e2358c71038c
Nikora Dmytro <dimanikora04@gmail.com>
2023-03-28 02:24:34 +0300

    Updated docker-compose file. Changed "localhost" in connection string to variable from environment. Added code to Program.cs for applying migrations at runtime. Added .gitignore to root folder for .env files.

--- server/Api/Program.cs
+++ server/Api/Program.cs
@@ -4,9 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-DotNetEnv.Env.Load("../.env");
-
-string connectionString = "Server=localhost,1433;" +
+string connectionString = $"Server={Environment.GetEnvironmentVariable("DB_HOST")}" +
                           $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
                           $"User Id={Environment.GetEnvironmentVariable("USER_ID")};" +
                           $"Password={Environment.GetEnvironmentVariable("SA_PASSWORD")};" +
@@ -23,6 +21,35 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await using (var context = scope.ServiceProvider.GetRequiredService<AppDbContext>())
+    {
+        try 
+        {
+            var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
+            var count = pendingMigrations.Count();
+            
+            if (count != 0)
+            {
+                Console.WriteLine($"You have {count} pending migrations to apply.");
+                Console.WriteLine("Applying pending migrations now");
+                await context.Database.MigrateAsync();
+            }
+
+            var appliedMigrations = await context.Database.GetAppliedMigrationsAsync();
+            var lastAppliedMigration = appliedMigrations.Last();
+
+            Console.WriteLine($"You're on schema version: {lastAppliedMigration}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to apply pending migrations!");
+            Console.WriteLine(ex);
+        }
+    }
+}
 
 // Configure the HTTP request pipeline.
 /* if (app.Environment.IsDevelopment())
