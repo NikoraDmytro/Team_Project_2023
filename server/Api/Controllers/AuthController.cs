2183354268f59d5c6c2fb55291bb4a3c53880b90
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2023-03-29 02:09:04 +0300

    Added AuthController

---
+++ server/Api/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using BLL.Interfaces;
+using BLL.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController: ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(
+        LoginModel loginModel, 
+        CancellationToken cancellationToken)
+    {
+        var tokenModel = await _authService.LoginAsync(loginModel, cancellationToken);
+        return Ok(tokenModel);
+    }
+
+    [HttpPost("signup")]
+    public async Task<IActionResult> Signup(
+        SignupModel signupModel,
+        CancellationToken cancellationToken)
+    {
+        var userModel = await _authService.SignupAsync(signupModel, cancellationToken);
+        return Ok(userModel);
+    }
+}
