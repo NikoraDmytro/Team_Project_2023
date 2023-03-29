01141db19b6bf940d32ca31ff90557cf44426bfe
Nikora Dmytro <dimanikora04@gmail.com>
2023-03-29 12:02:28 +0300

    Implemented extension classes for app and services. Moved all the necessary configurations to those classes keeping Program.cs clean.

---
+++ server/Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using DAL;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureSqlContext(this IServiceCollection services)
+    {
+
+        string connectionString = 
+                          $"Server={Environment.GetEnvironmentVariable("DB_HOST")}" +
+                          $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                          $"User Id={Environment.GetEnvironmentVariable("USER_ID")};" +
+                          $"Password={Environment.GetEnvironmentVariable("SA_PASSWORD")};" +
+                           "Encrypt=False";
+        
+        services.AddDbContext<AppDbContext>(opts =>
+            opts.UseSqlServer(connectionString, 
+                options => options.MigrationsAssembly("DAL")));
+    }
+}
--- server/Api/Program.cs
+++ server/Api/Program.cs
@@ -1,62 +1,16 @@
-using DAL;
-using Microsoft.EntityFrameworkCore;
+using Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string connectionString = $"Server={Environment.GetEnvironmentVariable("DB_HOST")}" +
-                          $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
-                          $"User Id={Environment.GetEnvironmentVariable("USER_ID")};" +
-                          $"Password={Environment.GetEnvironmentVariable("SA_PASSWORD")};" +
-                          "Encrypt=False";
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlServer(connectionString);
-});
-
+builder.Services.ConfigureSqlContext();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    await using (var context = scope.ServiceProvider.GetRequiredService<AppDbContext>())
-    {
-        try 
-        {
-            var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
-            var count = pendingMigrations.Count();
-            
-            if (count != 0)
-            {
-                Console.WriteLine($"You have {count} pending migrations to apply.");
-                Console.WriteLine("Applying pending migrations now");
-                await context.Database.MigrateAsync();
-            }
-
-            var appliedMigrations = await context.Database.GetAppliedMigrationsAsync();
-            var lastAppliedMigration = appliedMigrations.Last();
-
-            Console.WriteLine($"You're on schema version: {lastAppliedMigration}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Failed to apply pending migrations!");
-            Console.WriteLine(ex);
-        }
-    }
-}
-
-// Configure the HTTP request pipeline.
-/* if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}*/
 
 app.UseSwagger();
 app.UseSwaggerUI();
@@ -67,4 +21,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await app.MigrateDatabase();
 app.Run();
