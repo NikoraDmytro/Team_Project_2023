4ff6470775254c95c705def1fcd77a0a53fe9826
Nikora Dmytro <dimanikora04@gmail.com>
2023-03-30 19:14:19 +0300

    Created "Coach" entity. Added proper configuration for this entity. Created migration to add corresponding table to the database.

---
+++ server/Core/Entities/Coach.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Entities;
+
+public class Coach
+{
+    public int MembershipCardNum { get; set; }
+    public string? InstructorCategory { get; set; }
+    public string? Phone { get; set; }
+    
+    public int ClubId { get; set; }
+    public Club? Club { get; set; }
+    
+    //Every coach is a sportsman on its own
+    public Sportsman? Sportsman { get; set; }
+}
