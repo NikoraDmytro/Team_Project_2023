80b1572903e4b4363a7667991f271d7961795536
Iryna Bibik <bibik.iryna004@gmail.com>
2023-05-16 14:20:06 +0300

    add navigation bar and button exit

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
-import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import routes, { dashboardRoutes } from '../../const/routes';
+import './Header.scss';
+import logo from '../../assets/navIcons/taekwondo.png';
+import calendar from '../../assets/navIcons/calendar.png';
+import users from '../../assets/navIcons/users.png';
+import clubs from '../../assets/navIcons/clubs.png';
+import coaches from '../../assets/navIcons/coaches.png';
+import divisions from '../../assets/navIcons/divisions.png';
+import referees from '../../assets/navIcons/referees.png';
+import sportsman from '../../assets/navIcons/sportsman.png';
 
 const Header = (): JSX.Element => {
   const navigate = useNavigate();
@@ -10,43 +18,72 @@ const Header = (): JSX.Element => {
     navigate(routes.REGISTRATION);
   };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography
-            variant='h4'
-            component='div'
-            sx={{ flexGrow: 1, cursor: 'pointer' }}
-            onClick={() => {
-              navigate(routes.DASHBOARD + dashboardRoutes.HOME);
-            }}
-          >
-            Taekwondo
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
-            <Button
-              color='inherit'
-              onClick={() => {
-                navigate(routes.DASHBOARD + dashboardRoutes.CALENDAR);
-              }}
-            >
-              Calendar
-            </Button>
-            <Button
-              color='inherit'
-              onClick={() => {
-                navigate(routes.DASHBOARD + dashboardRoutes.USERS);
-              }}
-            >
-              Calendar
-            </Button>
-          </Box>
-          <Button color='inherit' size='large' onClick={clickHandler}>
-            Log out
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <div className='container'>
+      <div className='logo'>
+        <img src={logo} alt='logo' />
+        <h1
+          onClick={() => {
+            navigate(routes.DASHBOARD + dashboardRoutes.HOME);
+          }}
+        >
+          Taekwon-do
+        </h1>
+      </div>
+      <div className='navbar'>
+        <NavLink
+          to={`${routes.DASHBOARD}${dashboardRoutes.USERS}`}
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          <img src={users} alt='users-icon' />
+          <p>Користувачі</p>
+        </NavLink>
+        <NavLink
+          to={`${routes.DASHBOARD}${dashboardRoutes.CLUBS}`}
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          <img src={clubs} alt='clubs-icon' />
+          <p>Клуби</p>
+        </NavLink>
+        <NavLink
+          to={`${routes.DASHBOARD}${dashboardRoutes.SPORTSMAN}`}
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          <img src={sportsman} alt='sportsman-icon' />
+          <p>Спортсмени</p>
+        </NavLink>
+        <NavLink
+          to={`${routes.DASHBOARD}${dashboardRoutes.COACHES}`}
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          <img src={coaches} alt='coaches-icon' />
+          <p>Тренери</p>
+        </NavLink>
+        <NavLink
+          to={`${routes.DASHBOARD}${dashboardRoutes.REFEREES}`}
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          <img src={referees} alt='referees-icon' />
+          <p>Судді</p>
+        </NavLink>
+        <NavLink
+          to={`${routes.DASHBOARD}${dashboardRoutes.CALENDAR}`}
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          <img src={calendar} alt='calendar-icon' />
+          <p>Календар</p>
+        </NavLink>
+        <NavLink
+          to={`${routes.DASHBOARD}${dashboardRoutes.DIVISIONS}`}
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          <img src={divisions} alt='divisions-icon' />
+          <p>Дивізіони</p>
+        </NavLink>
+      </div>
+      <div className='exit'>
+        <p onClick={clickHandler}>Вийти</p>
+      </div>
+    </div>
   );
 };
 
